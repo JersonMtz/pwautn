f8ec3d9553adb9df32364c22f2a1cfb0c0fa39af
JersonMtz <gxnz_17@live.com.mx>
2021-02-15 21:48:55 -0600

    Interface de usuario

---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,11 @@
+export interface UserInterface {
+    id?:string;
+    uid?:string;
+    name:string;
+    surname?:string;
+    status:boolean;
+    role:boolean;
+    mail:string;
+    password:string;
+    photo?:string;
+}
