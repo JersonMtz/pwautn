d055a3c2210f76a5b80d9c6a4025a76c879ca9fe
JersonMtz <gxnz_17@live.com.mx>
2021-04-07 20:53:26 -0600

    Cambios componente stock

--- src/app/pages/components/stock/stock.component.ts
+++ src/app/pages/components/stock/stock.component.ts
@@ -1,32 +1,42 @@
-import { Component, Input, Output, EventEmitter, OnChanges, OnDestroy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { ProductInterface } from '@models/product.interface';
 import { MessagesService } from '@shared/services/messages.service';
 import { AfProductService } from '@pages/products/services/afProduct.service';
-import { Subscription } from 'rxjs';
+import { from, Subscription } from 'rxjs';
 import { PrimeNGConfig } from 'primeng/api';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'stock',
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss']
 })
-export class StockComponent implements OnChanges, OnDestroy {
+export class StockComponent implements OnInit, OnChanges, OnDestroy {
 
   private sub$: Subscription;
-  cols:any;
+  cols: any;
   @Input('delete') productDelete: ProductInterface;
   @Input('sale') typeAccion: boolean = true;
   @Output('order') productOut: EventEmitter<ProductInterface> = new EventEmitter();
-
 
   listProduct: ProductInterface[] = [];
 
   constructor(private afProduct: AfProductService,
     private primeng: PrimeNGConfig,
     private popup: MessagesService) {
-    this.sub$ = this.afProduct.list().subscribe(list => this.listProduct = list);
     this.primeng.ripple = true;
     this.header();
+  }
+
+  ngOnInit() {
+    this.sub$ = this.afProduct.list().subscribe(list => {
+      if (this.typeAccion) {
+        const data = from<ProductInterface[]>(list).pipe(filter(p => p.price > 0));
+        data.subscribe(res => this.listProduct.push(res));
+      } else {
+        this.listProduct = list;
+      }
+    });
   }
 
   ngOnChanges() {
@@ -52,7 +62,7 @@ export class StockComponent implements OnChanges, OnDestroy {
   stockError(amount: number, product: ProductInterface) {
     if ((amount > product.stock && this.typeAccion) || amount < 0 || !amount) {
       product.error = true;
-      this.popup.notification('error', '<span class="text-white">Verifique la cantidad del producto</span>','#E6242B');
+      this.popup.notification('error', '<span class="text-white">Verifique la cantidad del producto</span>', '#E6242B');
     } else {
       product.error = false;
     }
@@ -61,7 +71,7 @@ export class StockComponent implements OnChanges, OnDestroy {
   isNegative(amount: number, product: ProductInterface) {
     if (amount < 0 || !amount) {
       product.error = true;
-      this.popup.notification('error', '<span class="text-white">Verifique el precio del producto</span>','#E6242B');
+      this.popup.notification('error', '<span class="text-white">Verifique el precio del producto</span>', '#E6242B');
     } else {
       product.error = false;
       if (this.typeAccion) {
@@ -74,7 +84,7 @@ export class StockComponent implements OnChanges, OnDestroy {
 
   addToBill(amount: number, product: ProductInterface) {
     if (product.stock === 0 && this.typeAccion) {
-      this.popup.notification('info', `<span class="text-white">No hay  ${product.name.toUpperCase()} suficiente en stock</span>`,'#2799F3');
+      this.popup.notification('info', `<span class="text-white">No hay  ${product.name.toUpperCase()} suficiente en stock</span>`, '#2799F3');
       product.error = true;
     } else {
       if (amount && amount > 0) {
@@ -92,7 +102,7 @@ export class StockComponent implements OnChanges, OnDestroy {
         this.productOut.emit(tem);
         this.popup.notification('success', `<span class="text-white">Se agrego ${product.name.toUpperCase()} a la factura</span>`, '#52B256');
       } else {
-        this.popup.notification('info', `<span class="text-white">Verifique la cantidad del producto ${product.name.toUpperCase()}</span>`,'#2799F3');
+        this.popup.notification('info', `<span class="text-white">Verifique la cantidad del producto ${product.name.toUpperCase()}</span>`, '#2799F3');
       }
     }
   }
